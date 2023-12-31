b85e1ff74c707605b7d4de05a32e44f640410e7e
Elendil96 <marcin9617@gmail.com>
2023-12-31 14:45:59 +0100

    Day4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Basia";
+var sex = 'k';
+var age = 27;
+
+if (sex == 'k' && age < 30)
+{
+    if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa lat 30");
+    }
+    else
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+else if (sex == 'm' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
