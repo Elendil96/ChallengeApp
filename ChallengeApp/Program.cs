829af032d46ebcdc4e4e375c9502518a603032d0
Elendil96 <marcin9617@gmail.com>
2023-12-31 14:48:41 +0100

    Day4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -6,7 +6,7 @@ if (sex == 'k' && age < 30)
 {
     if (name == "Ewa" && age == 30)
     {
-        Console.WriteLine("Ewa lat 30");
+        Console.WriteLine("Ewa, lat 30");
     }
     else
     {
